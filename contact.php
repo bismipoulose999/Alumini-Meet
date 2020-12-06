5594daf18c4fdb01a81f851555155116aa2b9da2
bismipoulose999 <bismipoulose999@gmail.com>
2020-12-06 13:36:24 +0530

    page changed

--- contact.php
+++ contact.php
@@ -11,16 +11,16 @@
     <nav class="navbar navbar-expand-sm bg-light navbar-light">
         <ul class="navbar-nav">
           <li class="nav-item active">
-            <a class="nav-link" href="index.html">Login</a>
+            <a class="nav-link" href="index.php">Login</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="home.html">Home</a>
+            <a class="nav-link" href="home.php">Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="registration.html">Registration</a>
+            <a class="nav-link" href="registration.php">Registration</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="contact.html">Contact</a>
+            <a class="nav-link" href="contact.php">Contact</a>
           </li>
           
           
